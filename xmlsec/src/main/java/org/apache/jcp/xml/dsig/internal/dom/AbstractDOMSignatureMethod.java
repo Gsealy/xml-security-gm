2c8a40d8200f844db942fc408f54eba6ccb6470d
Gsealy <jiaojingwei1001@hotmail.com>
2018-02-12 11:15:54 +0800

    add ECSM2

--- xmlsec/src/main/java/org/apache/jcp/xml/dsig/internal/dom/AbstractDOMSignatureMethod.java
+++ xmlsec/src/main/java/org/apache/jcp/xml/dsig/internal/dom/AbstractDOMSignatureMethod.java
@@ -41,7 +41,7 @@ abstract class AbstractDOMSignatureMethod extends BaseStructure
     implements SignatureMethod {
 
     // denotes the type of signature algorithm
-    enum Type { DSA, RSA, ECDSA, HMAC }
+    enum Type { DSA, RSA, ECDSA, HMAC, ECSM2 }
 
     /**
      * Verifies the passed-in signature with the specified key, using the
