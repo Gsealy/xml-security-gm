8259cb195a7c9b73247a600ef0227a4f4283d419
Gsealy <jiaojingwei1001@hotmail.com>
2018-02-12 10:38:31 +0800

    add GMAlgorithmsSpec

--- xmlsec/src/main/java/org/apache/xml/security/utils/Constants.java
+++ xmlsec/src/main/java/org/apache/xml/security/utils/Constants.java
@@ -263,6 +263,10 @@ public final class Constants {
      */
     public static final String ALGO_ID_SIGNATURE_ECDSA_CERTICOM =
         "http://www.certicom.com/2000/11/xmlecdsig#ecdsa-sha1";
+    
+    /**GM - GMURI */
+    public static final String GMAlgorithmsSpec =
+    		"http://www.w3.org/2018/02/xmlgmdsig#";
 
     private Constants() {
         // we don't allow instantiation
